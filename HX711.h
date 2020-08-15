cbba23053088c1bf128c52aebcf97634669bcf4c
GreensCH <47106094+GreensCH@users.noreply.github.com>
2020-08-16 03:13:20 +0800

    Update HX711.h

--- HX711.h
+++ HX711.h
@@ -1,7 +1,7 @@
 #ifndef __HX711_H
 #define	__HX711_H
 
-/* 只需要修改下面的代码即可改变控制的LED引脚 */
+/* 只需要修改下面的代码即可改变控制的引脚 */
 #define ADDO_GPIO_PORT    	GPIOB			              /* GPIO端口 */
 #define ADDO_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO端口时钟 */
 #define ADDO_GPIO_PIN		GPIO_Pin_2			        /* 连接到SCL时钟线的GPIO */
